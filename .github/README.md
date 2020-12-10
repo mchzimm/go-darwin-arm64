bcf6f2c764348d951480833c77c2c5f3de13b675
mchzimm <75694877+mchzimm@users.noreply.github.com>
2020-12-10 00:38:33 -0500

    Add files via upload

---
+++ .github/README.md
@@ -0,0 +1,16 @@
+2020-12-9, MCH.ZIMM@GMAIL.COM
+
+
+"install-go-darwin-arm64.sh" - sets up a new working Golang dev environment built natively for Mac OS 11 "Big Sur" on Apple Metal / Darwin ARM64 platform ("Macbook Air M1" or similar, released  2020-11)
+
+Default paths should work, but edit accordingly as per your need.
+
+
+"update-go-darwin-arm64.sh" - just updates the previous version.
+
+If paths in installer batch file were modified, don't forget to also update in this one.
+
+
+TODO:
+
+Consolidate both files and just run update by checking if an existing version for Darwin ARM64 already exists.
---
+++ .github/update-go-darwin-arm64.sh
@@ -0,0 +1,13 @@
+export GO_TMP=$(mktemp -udt go-arm-bootstrap)
+mkdir -p $GO_TMP
+cd $GO_TMP
+curl -Ls https://golang.org/dl/go1.15.6.darwin-amd64.tar.gz | tar -xjf -
+git clone https://go.googlesource.com/go $GO_TMP/go-bootstrap
+cd $GO_TMP/go-bootstrap/src
+arch --x86_64 env GOROOT_BOOTSTRAP=$GO_TMP/go GODEBUG=asyncpreemptoff=1 GOOS=darwin GOARCH=arm64 ./bootstrap.bash
+export GO_RUNTIME=$GO_TMP/go-runtime
+git clone https://go.googlesource.com/go $GO_RUNTIME
+cd $GO_RUNTIME/src
+arch --arm64e env GOROOT_BOOTSTRAP=$GO_TMP/go-darwin-arm64-bootstrap ./make.bash
+sudo --preserve-env=GO_RUNTIME zsh -c 'mv $GO_RUNTIME /usr/local/opt/go && chown -R root:wheel /usr/local/opt/go'
+go version
